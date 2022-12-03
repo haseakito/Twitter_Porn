c6d43be10e1e1349b5af3844236ff8533848f956
ahas0037 <ahas0037@student.monash.edu>
2022-12-03 15:49:16 +0800

    implement queries by multiple accounts

--- get_tweets.py
+++ get_tweets.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 bearer_token = os.getenv("BEARER_TOKEN")
 
-def _create_url():
+def _create_urls():
     username = input("Type username: ")
 
     usernames = f"usernames={username}"
@@ -31,7 +31,7 @@ def _connect_to_endpoint(url):
     return res.json()
 
 def get_user_id():
-    url = _create_url()
+    url = _create_urls()
     json_res = _connect_to_endpoint(url)
     print(json.dumps(json_res, indent=4, ensure_ascii=False))
 
@@ -40,45 +40,54 @@ def get_user_id():
         ids.append(json_res["data"][num]["id"])
     return ids
 
-def create_url():
+def create_urls():
 
     expansions = "expansions=attachments.media_keys&media.fields=variants"
 
     ids = get_user_id()
 
+    urls = []
     for id in ids:
         url = f"https://api.twitter.com/2/users/{id}/tweets?{expansions}"
+        urls.append(url)
 
-    return url
+    return urls
 
 
-def connect_to_endpoint(url):
+def connect_to_endpoint(urls):
     header = {"Authorization" : f"Bearer {bearer_token}"}
 
-    res = requests.get(url, headers=header)
+    results = []
+    for url in urls:
+        res = requests.get(url, headers=header)
 
-    if res.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}" .format(
-                res.status_code, res.text
+        if res.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}" .format(
+                    res.status_code, res.text
+                )
             )
-        )
-    return res.json()
+
+        results.append(res.json())
+    return results
 
 def get_tweets():
-    url = create_url()
-    json_res = connect_to_endpoint(url)
-    print(json.dumps(json_res, indent=4, ensure_ascii=False))
-    
-    urls = []
-    for num in range(len(json_res["includes"]["media"])):
-        index = 0
-        while index < len(json_res["includes"]["media"][num]["variants"]):
-            if json_res["includes"]["media"][num]["variants"][index]["content_type"] == "video/mp4":
-                break
-            index += 1
-        urls.append(json_res["includes"]["media"][num]["variants"][index]["url"])
-    return urls
+    urls = create_urls()
+    results = connect_to_endpoint(urls)
+
+    porn_urls = []
+    for json_res in results:
+        print(json.dumps(json_res, indent=4, ensure_ascii=False))
+
+        for num in range(len(json_res["includes"]["media"])):
+            index = 0
+            while index < len(json_res["includes"]["media"][num]["variants"]):
+                if json_res["includes"]["media"][num]["variants"][index]["content_type"] == "video/mp4":
+                    break
+                index += 1
+            porn_urls.append(json_res["includes"]["media"][num]["variants"][index]["url"])
+
+    return porn_urls
 
 if __name__ == '__main__':
     print(get_tweets())
